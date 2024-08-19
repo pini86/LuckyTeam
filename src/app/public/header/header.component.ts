d8ce86f04cd1e77617d14f55b7ba63bce89d559b
letanatol <letanatol@gmail.com>
2024-08-19 16:49:10 +0300

    feat: add a layout for the header component

--- src/app/public/header/header.component.ts
+++ src/app/public/header/header.component.ts
@@ -1,10 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
