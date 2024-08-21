abf05a550f1c02c089ffc3c055409c24f67ed66c
letanatol <100472058+letanatol@users.noreply.github.com>
2024-08-21 23:30:31 +0300

    Header component (#2)
    * feat: add a layout for the header component
    
    * feat: add Guest, General user and Administrator states for header

--- src/app/public/header/header.component.ts
+++ src/app/public/header/header.component.ts
@@ -1,11 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../shared/auth.service';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, NgIf, MatMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  protected readonly _auth = inject(AuthService);
+}
