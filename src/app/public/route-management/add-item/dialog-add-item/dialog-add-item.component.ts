a1bdc826956cd8cafa210cc9ba7c40a6ec2e3f52
webjsmaster <web.js.master@gmail.com>
2024-08-24 11:55:06 +0300

    feat: add formGroup

--- src/app/public/route-management/add-item/dialog-add-item/dialog-add-item.component.ts
+++ src/app/public/route-management/add-item/dialog-add-item/dialog-add-item.component.ts
@@ -1,7 +1,16 @@
 import { CdkMonitorFocus } from '@angular/cdk/a11y';
 import { NgFor } from '@angular/common';
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import {
   MAT_DIALOG_DATA,
@@ -16,7 +25,7 @@ import { MatGridList, MatGridTile } from '@angular/material/grid-list';
 import { MatIcon } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
 import { MatOption, MatSelect } from '@angular/material/select';
-import { ModifyRoutesModel } from '../../../../shared/types/routes.model';
+import { CityModel, ModifyRoutesModel } from '../../../../shared/types/routes.model';
 import { DialogData } from '../../types/dialogs.types';
 
 @Component({
@@ -39,6 +48,7 @@ import { DialogData } from '../../types/dialogs.types';
     MatOption,
     MatIcon,
     NgFor,
+    ReactiveFormsModule,
   ],
   templateUrl: './dialog-add-item.component.html',
   styleUrl: './dialog-add-item.component.scss',
@@ -49,16 +59,43 @@ export class DialogAddItemComponent implements OnInit {
     path: [],
     carriages: [],
   });
+
+  protected readonly _formCities = new FormArray([]);
+  protected readonly _form = new FormGroup({
+    cities: this._formCities,
+  });
   private readonly _dialogRef = inject(MatDialogRef<DialogAddItemComponent>);
+  private readonly _fb = inject(FormBuilder);
   private readonly _data = inject<DialogData>(MAT_DIALOG_DATA);
 
   public ngOnInit(): void {
     if (this._data.route) {
       this._route.set(this._data.route);
+
+      this._route().path.forEach((path) => {
+        // const newControl = new FormControl(path.city, Validators.required);
+
+        this._formCities.push(this._createCities(path));
+      });
     }
   }
 
   public onNoClick(): void {
     this._dialogRef.close();
+  }
+
+  getFormGroup(control: AbstractControl) {
+    console.log('[79] 🌻:', control);
+    return control as FormControl;
+  }
+
+  protected _submit(): void {
+    console.log('[74] 🍄:', this._form.value);
+  }
+
+  private _createCities(path: CityModel): FormGroup {
+    return this._fb.group({
+      [path.city]: ['', Validators.required],
+    });
   }
 }
