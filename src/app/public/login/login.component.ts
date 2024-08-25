8b4896835dc43380fba6c4d4339d8f03bf6464c1
webjsmaster <web.js.master@gmail.com>
2024-08-25 22:49:23 +0300

    fix: eslint rules and refactor all for rules

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -12,6 +12,7 @@ import { AuthService } from '../../shared/services/auth.service';
 interface IResponse {
   token: string;
 }
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -21,20 +22,18 @@ interface IResponse {
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  public form = new FormGroup({
+    email: new FormControl<string>('', Validators.required),
+    password: new FormControl<string>({ value: '', disabled: true }, Validators.required),
+  });
+  protected readonly _isError = signal<string>('');
   private readonly _http = inject(HttpClient);
   private readonly _router = inject(Router);
   private readonly _auth = inject(AuthService);
 
-  readonly isError = signal<string>('');
-
-  form = new FormGroup({
-    email: new FormControl<string>('', Validators.required),
-    password: new FormControl<string>({ value: '', disabled: true }, Validators.required),
-  });
-
   constructor() {
     this.form.statusChanges.subscribe(() => {
-      this.isError.set('');
+      this._isError.set('');
       if (this.form.controls.email.invalid && this.form.controls.password.enabled) {
         this.form.controls.password.disable();
       } else if (this.form.controls.email.valid && this.form.controls.password.disabled) {
@@ -43,7 +42,7 @@ export class LoginComponent {
     });
   }
 
-  saveForm(): void {
+  public saveForm(): void {
     if (this.form.status === 'INVALID') {
       return;
     }
@@ -54,12 +53,13 @@ export class LoginComponent {
         this._router.navigate(['']); // адрес куда переходим после успешной авторизации
       },
       error: (error) => {
-        this.isError.set(error.error.message);
+        this._isError.set(error.error.message);
       },
     });
   }
 
-  signup(): void {//это тестовый запрос для образца использования токена
+  public signup(): void {
+    //это тестовый запрос для образца использования токена
     this._http
       .get('/api/station', {
         headers: {
@@ -74,7 +74,7 @@ export class LoginComponent {
       });
   }
 
-  admin(): void {
+  public admin(): void {
     this._http.post('/api/signin', { email: 'admin@admin.com', password: 'my-password' }).subscribe({
       next: (data) => {
         console.log(data);
