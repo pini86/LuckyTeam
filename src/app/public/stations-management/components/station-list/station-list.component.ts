c67aba7f503bc04db5df427cd97ea5f678dcefed
Anna Golosova <a.golosova@3l.ru>
2024-08-26 19:44:08 +0300

    feat: add list stations

--- src/app/public/stations-management/components/station-list/station-list.component.ts
+++ src/app/public/stations-management/components/station-list/station-list.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { RouteService } from '../../../../shared/services/route.service';
+import { CityModel, ConnectedStation } from '../../../../shared/types/routes.model';
+import { ConnectedStationsComponent } from '../connected-stations/connected-stations.component';
 
 @Component({
   selector: 'app-station-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatIconModule, MatButtonModule, ConnectedStationsComponent, MatListModule],
   templateUrl: './station-list.component.html',
   styleUrl: './station-list.component.scss',
 })
-export class StationListComponent {}
+export class StationListComponent implements OnInit {
+  public stations: CityModel[] = []; // Массив для хранения станций
+
+  constructor(
+    private routeService: RouteService,
+    private cd: ChangeDetectorRef,
+  ) {}
+
+  public ngOnInit(): void {
+    // Подписка на обновления списка станций
+    this.routeService.getCitiesObserver().subscribe((cities) => {
+      this.stations = cities.slice(0, 5); // Присваиваем первые 5 элементов напрямую
+      this.cd.detectChanges(); // Принудительно запускаем Change Detection
+    });
+
+    // Загрузка списка станций при инициализации компонента
+    this.routeService.getCities();
+  }
+
+  public trackByStationId(index: number, station: CityModel): number {
+    return station.id; // Уникальный идентификатор станции
+  }
+
+  // Функция trackBy для ConnectedStation (для подключенных станций)
+  public trackByConnectedStationId(index: number, station: ConnectedStation): number {
+    return station.id; // Уникальный идентификатор подключенной станции
+  }
+}
---
+++ src/app/shared/services/route.service.ts
@@ -0,0 +1,92 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { CitiesItems, CityModel, ConnectedStation, RideModel, RoutesItems, RoutesModel } from '../types/routes.model';
+import { AuthService } from './auth.service';
+
+@Injectable({ providedIn: 'root' })
+export class RouteService {
+  private readonly _http = inject(HttpClient);
+  private readonly _auth = inject(AuthService);
+  private readonly _routes = new Subject<RoutesItems>();
+  private readonly _cities = new Subject<CitiesItems>();
+  private readonly _route = new Subject<RideModel>();
+
+  private readonly _count = signal<number>(0);
+
+  public increment(): void {
+    this._count.update(() => this._count() + 1);
+  }
+
+  public getRoute(id: string): void {
+    this._http
+      .get<RideModel>(`/api/route/${id}`, {
+        headers: {
+          Authorization: `Bearer ${this._auth.getToken()}`,
+        },
+      })
+      .subscribe({
+        next: (data) => {
+          const newRoute = new RideModel(data.id, data.carriages, data.path, data.schedule);
+          this._route.next(newRoute);
+        },
+        error: (error) => console.log(error),
+      });
+  }
+
+  public getRoutes(): void {
+    this._http
+      .get<RoutesItems>('/api/route', {
+        headers: {
+          Authorization: `Bearer ${this._auth.getToken()}`,
+        },
+      })
+      .subscribe({
+        next: (data) => {
+          const arrRoutes: RoutesItems = [];
+          data.forEach((item) => {
+            const newItem = new RoutesModel(item.id, item.carriages, item.path);
+            arrRoutes.push(newItem);
+          });
+          this._routes.next(arrRoutes);
+        },
+        error: (error) => console.log(error),
+      });
+  }
+
+  public getCities(): void {
+    this._http
+      .get<CitiesItems>('/api/station', {
+        headers: {
+          Authorization: `Bearer ${this._auth.getToken()}`,
+        },
+      })
+      .subscribe({
+        next: (data) => {
+          const arrCities: CitiesItems = [];
+          data.forEach((city) => {
+            // Проверяем, есть ли данные по connectedTo и преобразуем их
+            const connectedStations = city.connectedTo
+              ? city.connectedTo.map((connection) => new ConnectedStation(connection.id, connection.distance))
+              : [];
+            const newCity = new CityModel(city.id, city.city, city.latitude, city.longitude, connectedStations);
+            arrCities.push(newCity);
+          });
+          this._cities.next(arrCities);
+        },
+        error: (error) => console.log(error),
+      });
+  }
+
+  public getRoutesObserver(): Observable<RoutesItems> {
+    return this._routes.asObservable();
+  }
+
+  public getRouteObserver(): Observable<RideModel> {
+    return this._route.asObservable();
+  }
+
+  public getCitiesObserver(): Observable<CitiesItems> {
+    return this._cities.asObservable();
+  }
+}
