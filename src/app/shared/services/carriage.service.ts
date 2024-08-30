77b3524f264e71ecd777b7b48257aa985e638bd7
letanatol <100472058+letanatol@users.noreply.github.com>
2024-08-30 10:38:29 +0300

    Carriages cars component (#7)
    * feat: add layout for carriages-cars
    
    * fix: update buildCarriageToVM for render array carriages
    
    * feat: implement update carriage
    
    * feat: add Validators.max for creat carriages form
    
    * feat: add responsive design and layout for mobile

---
+++ src/app/shared/services/carriage.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarriageService {
+  // public savedToken: string;
+  // public carriagesFromResponse = signal<CarriageVM[]>([]);
+
+  // constructor(private readonly http: HttpClient, private readonly auth: AuthService,) {
+  //   this.savedToken = localStorage.getItem('token');
+  //   this.getCarriages();
+  // }
+
+  // public getCarriages(): void {
+  //   this.http
+  //     .get<ICarriage[]>('/api/carriage', {
+  //       headers: {
+  //         Authorization: `Bearer ${this.savedToken}`,
+  //       },
+  //     })
+  //     .subscribe({
+  //       next: (data) => {
+  //         this.carriagesFromResponse.set(data);
+  //       },
+  //       error: (error) => console.error(error),
+  //     });
+  // }
+}
