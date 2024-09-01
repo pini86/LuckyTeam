f5b0bb71b0ed100f72f5931fe8c170c6960c3c2c
webjsmaster <web.js.master@gmail.com>
2024-09-01 22:31:47 +0300

    feat: add form fields

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideAnimations()],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimations(),
+    provideNativeDateAdapter(),
+  ],
 };
--- src/app/public/search/search.component.ts
+++ src/app/public/search/search.component.ts
@@ -1,11 +1,45 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatDatepicker, MatDatepickerInput, MatDatepickerToggle, MatDateRangeInput } from '@angular/material/datepicker';
+import { MatFormField, MatLabel, MatSuffix } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-search',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [
+    MatFormField,
+    MatInput,
+    MatLabel,
+    ReactiveFormsModule,
+    MatIcon,
+    MatDatepickerToggle,
+    MatDatepicker,
+    MatDatepickerInput,
+    MatSuffix,
+    MatDateRangeInput,
+    MatButton,
+    FormsModule,
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
 })
-export class SearchComponent {}
+export class SearchComponent implements OnInit {
+  protected _form = new FormGroup({
+    from: new FormControl<string>('', [Validators.required]),
+    to: new FormControl<string>('', Validators.required),
+    date: new FormControl<string>('', Validators.required),
+  });
+  protected readonly _minDate = new Date();
+
+  public ngOnInit(): void {
+    console.log('[33] 🎯: ');
+  }
+
+  protected _submit(): void {
+    console.log('[42] 🚧:', this._form.value);
+  }
+}
