de234674f20f0cd6f32a14bde31cbbe3e61bc67e
Anna Golosova <a.golosova@3l.ru>
2024-09-02 16:11:27 +0300

    feat: add routes service

---
+++ src/app/public/stations-management/types/routes.model.ts
@@ -0,0 +1,54 @@
+export class RoutesModel {
+  constructor(
+    public readonly id: number,
+    public readonly carriages: string[],
+    public readonly path: number[],
+  ) {}
+}
+
+export class ModifyRoutesModel {
+  constructor(
+    public readonly id: number,
+    public readonly carriages: string[],
+    public readonly path: CityModel[],
+  ) {}
+}
+
+export class ConnectedStation {
+  constructor(
+    public readonly id: number,
+    public readonly distance: number,
+  ) {}
+}
+
+export class CityModel {
+  constructor(
+    public readonly id: number,
+    public readonly city: string,
+    public readonly latitude: number,
+    public readonly longitude: number,
+    public readonly connectedTo: ConnectedStation[],
+  ) {}
+}
+
+export class RideModel {
+  constructor(
+    public readonly id: number,
+    public readonly carriages: string[],
+    public readonly path: number[],
+    public readonly schedule: Schedule[],
+  ) {}
+}
+
+export interface Schedule {
+  readonly rideId: number;
+  readonly segments: Segments[];
+}
+
+export interface Segments {
+  readonly price: Record<string, number>;
+  readonly time: string[];
+}
+
+export type RoutesItems = RoutesModel[];
+export type CitiesItems = CityModel[];
