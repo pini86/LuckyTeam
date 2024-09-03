1c3f778fe0c39e375298bceb47c83b77b5cf38f1
webjsmaster <web.js.master@gmail.com>
2024-09-03 20:55:11 +0300

    feat: add search result component

--- src/app/public/search/search.component.ts
+++ src/app/public/search/search.component.ts
@@ -1,17 +1,18 @@
+import { AsyncPipe, NgFor } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatAutocomplete, MatAutocompleteTrigger, MatOption } from '@angular/material/autocomplete';
 import { MatButton } from '@angular/material/button';
 import { MatDatepicker, MatDatepickerInput, MatDatepickerToggle, MatDateRangeInput } from '@angular/material/datepicker';
 import { MatFormField, MatLabel, MatSuffix } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
+import { map, Observable, startWith } from 'rxjs';
 import { RouteService } from '../../shared/services/route.service';
+import { SearchService } from '../../shared/services/search.service';
 import { StateService } from '../../shared/services/state.service';
 import { CityModel } from '../../shared/types/routes.model';
-import { map, Observable, startWith } from 'rxjs';
-import { MatAutocomplete, MatAutocompleteTrigger, MatOption } from '@angular/material/autocomplete';
-import { AsyncPipe, NgFor } from '@angular/common';
-import { SearchService } from '../../shared/services/search.service';
+import { SearchResultComponent } from './search-result/search-result.component';
 
 @Component({
   selector: 'app-search',
@@ -34,16 +35,18 @@ import { SearchService } from '../../shared/services/search.service';
     MatOption,
     NgFor,
     AsyncPipe,
-    MatAutocompleteTrigger
+    MatAutocompleteTrigger,
+    SearchResultComponent,
   ],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.scss'
+  styleUrl: './search.component.scss',
 })
 export class SearchComponent implements OnInit {
   protected _form = new FormGroup({
     from: new FormControl<string>(null, [Validators.required]),
     to: new FormControl<string>(null, Validators.required),
-    date: new FormControl<string>('', Validators.required)
+    date: new FormControl<string>('', Validators.required),
+    time: new FormControl<string>({ value: '', disabled: true }),
   });
   protected readonly _minDate = new Date();
   protected _filteredOptionsFrom: Observable<CityModel[]>;
@@ -52,18 +55,19 @@ export class SearchComponent implements OnInit {
   private readonly _searchService: SearchService = inject(SearchService);
   private readonly _stateService: StateService = inject(StateService);
   protected readonly _cities = this._stateService.cities;
+  protected readonly _searchRoutes = this._stateService.searchRoutes;
 
   public ngOnInit(): void {
     this._routeService.getCities();
 
     this._filteredOptionsFrom = this._form.controls.from.valueChanges.pipe(
       startWith(''),
-      map(val => this._filter(val))
+      map((val) => this._filter(val)),
     );
 
     this._filteredOptionsTo = this._form.controls.to.valueChanges.pipe(
       startWith(''),
-      map(val => this._filter(val))
+      map((val) => this._filter(val)),
     );
   }
 
@@ -71,8 +75,8 @@ export class SearchComponent implements OnInit {
     const valueFrom = this._form.controls.from.value;
     const valueTo = this._form.controls.to.value;
 
-    const fromCity = this._cities().find(city => city.city === valueFrom);
-    const toCity = this._cities().find(city => city.city === valueTo);
+    const fromCity = this._cities().find((city) => city.city === valueFrom);
+    const toCity = this._cities().find((city) => city.city === valueTo);
 
     if (!fromCity) {
       this._form.controls.from.setErrors({ error: 'invalid' });
@@ -87,22 +91,44 @@ export class SearchComponent implements OnInit {
       const fromLongitude = fromCity.longitude;
       const toLatitude = toCity.latitude;
       const toLongitude = toCity.longitude;
-      const time = new Date(this._form.value.date).getTime();
+
+      let dateTimestamp: number;
+
+      if (this._form.value.time) {
+        const date = new Date(this._form.value.date);
+        const time = this._form.value.time.split(':');
+
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        const hour = time[0];
+        const minutes = time[1];
+
+        dateTimestamp = new Date(year, month, day, Number(hour), Number(minutes)).getTime() / 1000;
+      } else {
+        dateTimestamp = new Date(this._form.value.date).getTime() / 1000;
+      }
 
       console.table({
         fromLatitude,
         fromLongitude,
         toLatitude,
         toLongitude,
-        time
       });
 
+      this._searchService.getSection({ fromLatitude, fromLongitude, toLongitude, toLatitude, time: dateTimestamp });
+    }
+  }
 
-      this._searchService.getSection({ fromLatitude, fromLongitude, toLongitude, toLatitude, time });
+  protected _handleChangeDate(): void {
+    if (this._form.controls.date.valid) {
+      this._form.controls.time.enable();
+    } else {
+      this._form.controls.time.disable();
     }
   }
 
   private _filter(val: string): CityModel[] {
-    return this._cities().filter(model => model.city.toLowerCase().startsWith(val.toLowerCase()));
+    return this._cities().filter((model) => model.city.toLowerCase().startsWith(val.toLowerCase()));
   }
 }
--- src/app/shared/services/search.service.ts
+++ src/app/shared/services/search.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { SearchRoutesModel } from '../types/search-routes.model';
 import { AuthService } from './auth.service';
 import { StateService } from './state.service';
 
@@ -9,24 +10,36 @@ export class SearchService {
   private readonly _auth = inject(AuthService);
   private readonly _stateService = inject(StateService);
 
-
-  public getSection({ fromLatitude, fromLongitude, toLatitude, toLongitude, time }: { fromLatitude: number, fromLongitude: number, toLatitude: number, toLongitude: number, time: number }): void {
+  public getSection({
+    fromLatitude,
+    fromLongitude,
+    toLatitude,
+    toLongitude,
+    time,
+  }: {
+    fromLatitude: number;
+    fromLongitude: number;
+    toLatitude: number;
+    toLongitude: number;
+    time: number;
+  }): void {
     this._http
-      .get('/api/search', {
+      .get<SearchRoutesModel>('/api/search', {
         params: {
           fromLatitude,
           fromLongitude,
           toLatitude,
           toLongitude,
-          time
-        }
+          time,
+        },
       })
       .subscribe({
         next: (data) => {
           console.log('🌻:response', data);
-
+          const newSearchRoutes = new SearchRoutesModel(data.from, data.to, data.routes);
+          this._stateService.setSearchRoutes(newSearchRoutes);
         },
-        error: (error) => console.error(error)
+        error: (error) => console.error(error),
       });
   }
 }
