27446da4982738a4ebfdd07d07c25ee84a91e599
Anna Golosova <a.golosova@3l.ru>
2024-09-16 15:02:57 +0300

    feat: add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { FooterComponent } from './public/footer/footer/footer.component';
 import { HeaderComponent } from './public/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, RouterLink],
+  imports: [RouterOutlet, HeaderComponent, RouterLink, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
